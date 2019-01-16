beeb03de99b51e24a720b42ce89e86ce0c02ff90
Andrea Tupini <tupini07@gmail.com>
2019-01-16 20:08:41 +0100

    some comments

--- bot_interface/helper.py
+++ bot_interface/helper.py
@@ -3,3 +3,6 @@ import sys
 
 sys.path.insert(0, '..')
 import config
+
+# The torrent service shouldn't talk with the database service. 
+# The bot is the one that consumes both services and is the one that should integrate them
